86ba339d568d262c69513b637d0f33e50ac44d13
Shinda <123310732+Sh1nosh1@users.noreply.github.com>
2023-12-14 13:42:59 +0200

    Add files via upload

---
+++ T13_1/Phone.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace T13_1
+{
+    class Phone
+    {
+        /// <summary>
+        /// Phone's manufacturer
+        /// </summary>
+        public string manufacturer;
+        /// <summary>
+        /// Phone's model
+        /// </summary>
+        public string model;
+        /// <summary>
+        /// Phone's price
+        /// </summary>
+        public double price;
+        /// <summary>
+        /// Constructor without parameters
+        /// </summary>
+        public Phone () { }
+        /// <summary>
+        /// Constructor with two parameters
+        /// </summary>
+        /// <param name="manufacturer"></param>
+        /// <param name="model"></param>
+        public Phone (string manufacturer, string model)
+        {
+            this.manufacturer = manufacturer; this.model = model;
+        }
+        /// <summary>
+        /// Constructor +1 parameter
+        /// </summary>
+        /// <param name="manufacturer"></param>
+        /// <param name="model"></param>
+        /// <param name="price"></param>
+        public Phone (string manufacturer, string model, double price) : this(manufacturer, model)
+        {
+            this.price = price;
+        }
+        /// <summary>
+        /// Output
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return $"Manufacturer: {manufacturer}\nModel: {model}\nPrice: {price} $\n";
+        }
+    }
+}
---
+++ T13_1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace T13_1
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+                
+                Phone phone1 = new Phone();
+                phone1.manufacturer = "Apple";
+                phone1.model = "Iphone 14 Pro Max";
+                phone1.price = 1700;
+                WriteLine($"{phone1.ToString()}");
+                Phone phone2 = new Phone("Samsung","Galaxy S22 Ultra",1550);
+                WriteLine($"{phone2.ToString()}");
+                Write("Введите прозводителя: ");
+                string manufacturer = ReadLine();
+                Write("Введите модель: ");
+                string model = ReadLine();
+                Write("Введите стоимость: ");
+                double price = Convert.ToDouble(ReadLine());
+                Phone phone3 = new Phone(manufacturer,model,price);
+                WriteLine($"\n{phone3.ToString()}");
+
+
+            }
+            catch (Exception ex)
+            {
+                WriteLine($"Исключение: {ex.Message}");
+                WriteLine($"Метод: {ex.TargetSite}");
+                WriteLine($"Трассировка стека: {ex.StackTrace}");
+            }
+            ReadKey();
+        }
+    }
+}
